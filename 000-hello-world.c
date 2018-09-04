7d0d9c2671ca4f4b56aa20667598b9b4d9ef16ef
carojr <carojr.yang@gmail.com>
2018-09-04 23:05:20 +0800

    Finish environment setting
    Setup githup and xcode, upload hello world to cellebrate environment setting done!!

---
+++ 000-hello-world.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  leetcode
+//
+//  Created by BurnburnYang on 2018/9/4.
+//  Copyright © 2018年 BurnburnYang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
